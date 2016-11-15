bcb6e4762bbee7799de9719b8d1991cd6d121e9c
javiercarvajalramirez <tibon_27@hotmail.com>
2016-11-15 08:45:27 -0400

    Create programa.py

---
+++ programa.py
@@ -0,0 +1,10 @@
+def comprueba (X,L): 
+  if x in L:
+    return false 
+  else: return true
+
+def genera (): l=range [1,42] mi_lista=[] while len (mi_lista)<6: numero=random.choice(l)
+
+if not (numero) in mi_lista: mi_lista.append(numero)
+
+return mi_lista
